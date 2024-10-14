ff4883ffba275de589b65559ca08a96bc7b87840
AndersonBH16 <ander.bh.16@gmail.com>
2024-10-14 11:57:10 -0500

    Setup Transparencia page

---
+++ src/util/info_uni.js
@@ -0,0 +1,55 @@
+export const ley_universitaria = [
+    {
+        "id"         : 1,
+        "descripcion": 'El Estatuto, el Texto Único de Procedimientos Administrativos (TUPA), el Plan Estratégico Institucional y el reglamento de la universidad.',
+        "icon"       : "fa-regular fa-folder-open"
+    },
+    {
+        "id"         : 2,
+        "descripcion": "Las actas aprobadas en las sesiones de Consejo de Facultad, de Consejo Universitario y de \n"
+                       +"Asamblea Universitaria.",
+        "icon"       : 'fa-solid fa-file-lines'
+    },
+    {
+        "id"         : 3,
+        "descripcion": "Los estados financieros de la universidad, el presupuesto institucional modificado en el caso \n"
+                       +"de las universidades públicas, la actualización de la ejecución presupuestal y balances.",
+        "icon"       : 'fa-solid fa-coins'
+    },
+    {
+        "id"         : 4,
+        "descripcion": "Relación y número de becas y créditos educativos disponibles y otorgados en el año en curso.",
+        "icon"       : 'fa-solid fa-hand-holding-dollar'
+    },
+    {
+        "id"         : 5,
+        "descripcion": "Inversiones, reinversiones, donaciones, obras de infraestructura, recursos de diversa fuente,\n"
+        +"entre otros",
+        "icon"       : 'fa-solid fa-sack-dollar'
+    },
+    {
+        "id"         : 6,
+        "descripcion": "Proyectos de investigación y los gastos que genere.",
+        "icon"       : 'fa-brands fa-searchengin'
+    },
+    {
+        "id"         : 7,
+        "descripcion": "Relación de pagos exigidos a los alumnos por toda índole, según corresponda.",
+        "icon"       : 'fa-solid fa-file-invoice-dollar'
+    },
+    {
+        "id"         : 8,
+        "descripcion": "Número de alumnos por facultades y programas de estudio.",
+        "icon"       : 'fa-solid fa-graduation-cap'
+    },
+    {
+        "id"         : 9,
+        "descripcion": "Conformación del cuerpo docente, indicando clase, categoría y hoja de vida.",
+        "icon"       : 'fa-solid fa-chalkboard-user'
+    },
+    {
+        "id"         : 10,
+        "descripcion": "El número de postulantes, ingresantes, matriculados y egresados por año y carrera.",
+        "icon"       : 'fa-solid fa-list-ol'
+    }
+];
