aafa93bf2aad94b987f2bb7139e0ffdbd64c661f
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-04-14 14:20:37 -0500

    Project structure has been modified; all required files have been included in public folder; some components have been renamed

---
+++ src/util/data-numeral-11.8/info_11.8.ts
@@ -0,0 +1,15 @@
+import type { NumeralInformation } from "../../interfaces";
+
+export const data_numeral_11_8: NumeralInformation[] = [
+  {
+    id: 1,
+    label: "Número de Alumnos en la Facultad de Estomatología en el año 2019.",
+    lawInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    fileButtonTooltip:
+      "Descargar Número de Alumnos para Segunda Especialidad en la Facultad de Estomatología 2019",
+    filePath: "/files/numeral-11.8/2019/nro_alumnos_estomatologia_2019.pdf",
+    icon: "fa-solid fa-school",
+    year: "2019",
+  },
+];
