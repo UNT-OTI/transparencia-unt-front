ac993dc37bc72abcd99f1168f6e825ebba27563b
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-04-23 13:37:49 -0500

    Numeral 11.8 has been updated; pending second specialty

--- src/util/data-numeral-11.8/info_11.8.ts
+++ src/util/data-numeral-11.8/info_11.8.ts
@@ -4,12 +4,37 @@ export const data_numeral_11_8: NumeralInformation[] = [
   {
     id: 1,
     lawDescription:
-      "Número de Alumnos en la Facultad de Estomatología en el año 2019.",
+      "Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2019.",
     lawSubInformation:
       "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
     buttonTooltip:
-      "Descargar Número de Alumnos para Segunda Especialidad en la Facultad de Estomatología 2019",
-    filePath: "/files/numeral-11.8/2019/nro_alumnos_estomatologia_2019.pdf",
+      "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2019",
+    filePath: "/files/numeral-11.8/2019/alumnos_pregrado_2019.pdf",
+    icon: "fa-solid fa-school",
+    buttonDescription: "2019",
+    hasTargetBlank: true,
+  },
+  {
+    id: 2,
+    lawDescription:
+      "Número de Estudiantes de Posgrado Matriculados – Maestría 2019.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    buttonTooltip:
+      "Descargar Número de Estudiantes de Posgrado Matriculados – Maestría 2019",
+    filePath: "/files/numeral-11.8/2019/alumnos_posgrado_maestria_2019.pdf",
+    icon: "fa-solid fa-school",
+    buttonDescription: "2019",
+    hasTargetBlank: true,
+  },
+  {
+    id: 3,
+    lawDescription: "Número de estudiantes de Posgrado - Doctorados 2019.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    buttonTooltip:
+      "Descargar Número de estudiantes de Posgrado - Doctorados 2019",
+    filePath: "/files/numeral-11.8/2019/alumnos_posgrado_doctorado_2019.pdf",
     icon: "fa-solid fa-school",
     buttonDescription: "2019",
     hasTargetBlank: true,
