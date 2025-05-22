7cf7e497c13c3838a066a262ece5f8e595b49da9
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-05-22 10:18:30 -0500

    Information for numeral 11.4 has been uploaded

--- src/util/data-numeral-11.4/info_11.4.ts
+++ src/util/data-numeral-11.4/info_11.4.ts
@@ -4,27 +4,14 @@ export const data_numeral_11_4: NumeralInformation[] = [
   {
     id: 1,
     lawDescription:
-      "Relación y Número de Becas disponibles y otorgadas para los programas de segunda especialidad de Estomatología en el año 2019.",
+      "Relación y Número de Becas Disponibles y Otorgadas para los programas de Segunda Especialidad en el año 2019.",
     lawSubInformation:
       "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.4",
     buttonTooltip:
-      "Descargar Relación y Número de Becas Disponibles y Otorgadas Estomatología 2019",
-    filePath: "/files/numeral-11.4/2019/becas_estomatologia_2019.pdf",
+      "Descargar Relación y Número de Becas Disponibles y Otorgadas - 2019",
+    filePath: "/files/numeral-11.4/2019/becas_segundas_especialidades_2019.pdf",
     icon: "fa-solid fa-graduation-cap",
     buttonDescription: "2019",
-    hasTargetBlank: true,
-  },
-  {
-    id: 2,
-    lawDescription:
-      "Relación y Número de Becas disponibles y otorgadas para los programas de segunda especialidad de Medicina en el año 2022.",
-    lawSubInformation:
-      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.4",
-    buttonTooltip:
-      "Descargar Relación y Número de Becas Disponibles y Otorgadas Medicina 2022",
-    filePath: "/files/numeral-11.4/2022/becas_medicina_2022.pdf",
-    icon: "fa-solid fa-graduation-cap",
-    buttonDescription: "2022",
     hasTargetBlank: true,
   },
 ];
