f17af1d25e581edd7956d2c01e95cb2eaeffa8fc
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-05-23 13:48:31 -0500

    undergraduate infromation

--- src/util/data-numeral-11.8/info_11.8.ts
+++ src/util/data-numeral-11.8/info_11.8.ts
@@ -4,6 +4,91 @@ export const data_numeral_11_8: StatementInformation[] = [
   {
     id: 1,
     lawDescription:
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2024.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    icon: "fa-solid fa-school",
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2024",
+        filePath: "/files/numeral-11.8/2024/alumnos_pregrado_2024.pdf",
+        hasTargetBlank: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    lawDescription:
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2023.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    icon: "fa-solid fa-school",
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2023",
+        filePath: "/files/numeral-11.8/2023/alumnos_pregrado_2023.pdf",
+        hasTargetBlank: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    lawDescription:
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2022.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    icon: "fa-solid fa-school",
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2022",
+        filePath: "/files/numeral-11.8/2022/alumnos_pregrado_2022.pdf",
+        hasTargetBlank: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    lawDescription:
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2021.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    icon: "fa-solid fa-school",
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2021",
+        filePath: "/files/numeral-11.8/2021/alumnos_pregrado_2021.pdf",
+        hasTargetBlank: true,
+      },
+    ],
+  },
+  {
+    id: 5,
+    lawDescription:
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2020.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    icon: "fa-solid fa-school",
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2020",
+        filePath: "/files/numeral-11.8/2020/alumnos_pregrado_2020.pdf",
+        hasTargetBlank: true,
+      },
+    ],
+  },
+  {
+    id: 6,
+    lawDescription:
       "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2019.",
     lawSubInformation:
       "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
