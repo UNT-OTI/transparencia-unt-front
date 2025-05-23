969a88daa99d55c3bdb2a540e1495831ab68f4bb
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-05-23 13:34:04 -0500

    Design of page for numeral 11.8 has been modified

--- src/util/data-numeral-11.8/info_11.8.ts
+++ src/util/data-numeral-11.8/info_11.8.ts
@@ -1,56 +1,44 @@
-import type { NumeralInformation } from "@interfaces/index";
+import type { StatementInformation } from "@interfaces/index";
 
-export const data_numeral_11_8: NumeralInformation[] = [
+export const data_numeral_11_8: StatementInformation[] = [
   {
     id: 1,
     lawDescription:
-      "Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2019.",
+      "Número de Estudiantes Matriculados por Facultad y Programa de Estudio – 2019.",
     lawSubInformation:
       "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
-    buttonTooltip:
-      "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2019",
-    filePath: "/files/numeral-11.8/2019/alumnos_pregrado_2019.pdf",
     icon: "fa-solid fa-school",
-    buttonDescription: "2019",
-    hasTargetBlank: true,
-  },
-  {
-    id: 2,
-    lawDescription:
-      "Número de Estudiantes de Posgrado Matriculados – Maestría 2019.",
-    lawSubInformation:
-      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
-    buttonTooltip:
-      "Descargar Número de Estudiantes de Posgrado Matriculados – Maestría 2019",
-    filePath: "/files/numeral-11.8/2019/alumnos_posgrado_maestria_2019.pdf",
-    icon: "fa-solid fa-school",
-    buttonDescription: "2019",
-    hasTargetBlank: true,
-  },
-  {
-    id: 3,
-    lawDescription:
-      "Número de estudiantes de Posgrado Matriculados - Doctorados 2019.",
-    lawSubInformation:
-      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
-    buttonTooltip:
-      "Descargar Número de estudiantes de Posgrado - Doctorados 2019",
-    filePath: "/files/numeral-11.8/2019/alumnos_posgrado_doctorado_2019.pdf",
-    icon: "fa-solid fa-school",
-    buttonDescription: "2019",
-    hasTargetBlank: true,
-  },
-  {
-    id: 4,
-    lawDescription:
-      "Número de estudiantes de Segunda Especialidad Matriculados 2019.",
-    lawSubInformation:
-      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
-    buttonTooltip:
-      "Descargar Número de Estudiantes de Segunda Especialidad Matriculados por Facultad y Programa de Estudio – 2019",
-    filePath: "/files/numeral-11.8/2019/alumnos_segunda_especialidad_2019.pdf",
-    icon: "fa-solid fa-school",
-    buttonDescription: "2019",
-    hasTargetBlank: true,
+    rangeStatementItem: [
+      {
+        buttonDescription: "Pregrado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Pregrado Matriculados por Facultad y Programa de Estudio – 2019",
+        filePath: "/files/numeral-11.8/2019/alumnos_pregrado_2019.pdf",
+        hasTargetBlank: true,
+      },
+      {
+        buttonDescription: "Maestría",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Posgrado Matriculados – Maestría 2019",
+        filePath: "/files/numeral-11.8/2019/alumnos_posgrado_maestria_2019.pdf",
+        hasTargetBlank: true,
+      },
+      {
+        buttonDescription: "Doctorado",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Posgrado Matriculados - Doctorado 2019",
+        filePath:
+          "/files/numeral-11.8/2019/alumnos_posgrado_doctorado_2019.pdf",
+        hasTargetBlank: true,
+      },
+      {
+        buttonDescription: "Seg. Especialidad",
+        buttonTooltip:
+          "Descargar Número de Estudiantes de Segunda Especialidad Matriculados por Facultad y Programa de Estudio – 2019",
+        filePath:
+          "/files/numeral-11.8/2019/alumnos_segunda_especialidad_2019.pdf",
+        hasTargetBlank: true,
+      },
+    ],
   },
 ];
