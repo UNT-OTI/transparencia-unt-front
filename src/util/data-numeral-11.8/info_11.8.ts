15820eac4a371542ba717dc5467710eebcd3a3b7
Roberto Núñez <rjnunezc@unitru.edu.com>
2025-05-23 13:17:47 -0500

    Adding information for second specialty

--- src/util/data-numeral-11.8/info_11.8.ts
+++ src/util/data-numeral-11.8/info_11.8.ts
@@ -29,12 +29,26 @@ export const data_numeral_11_8: NumeralInformation[] = [
   },
   {
     id: 3,
-    lawDescription: "Número de estudiantes de Posgrado - Doctorados 2019.",
+    lawDescription:
+      "Número de estudiantes de Posgrado Matriculados - Doctorados 2019.",
     lawSubInformation:
       "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
     buttonTooltip:
       "Descargar Número de estudiantes de Posgrado - Doctorados 2019",
     filePath: "/files/numeral-11.8/2019/alumnos_posgrado_doctorado_2019.pdf",
+    icon: "fa-solid fa-school",
+    buttonDescription: "2019",
+    hasTargetBlank: true,
+  },
+  {
+    id: 4,
+    lawDescription:
+      "Número de estudiantes de Segunda Especialidad Matriculados 2019.",
+    lawSubInformation:
+      "Transparencia de Universidades – Ley 30220 - Artículo 11 - Numeral 11.8",
+    buttonTooltip:
+      "Descargar Número de Estudiantes de Segunda Especialidad Matriculados por Facultad y Programa de Estudio – 2019",
+    filePath: "/files/numeral-11.8/2019/alumnos_segunda_especialidad_2019.pdf",
     icon: "fa-solid fa-school",
     buttonDescription: "2019",
     hasTargetBlank: true,
